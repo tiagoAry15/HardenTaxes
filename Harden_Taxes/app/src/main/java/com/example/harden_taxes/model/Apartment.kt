d3b7f305ba1e385dd2c256d1c740bf99ff15081a
Tiago Ary Castelo <tiago.ary10@gmail.com>
2021-05-26 22:23:36 -0300

    data classes e banco de dados

--- Harden_Taxes/app/src/main/java/com/example/harden_taxes/model/Apartment.kt
+++ Harden_Taxes/app/src/main/java/com/example/harden_taxes/model/Apartment.kt
@@ -1,3 +1,9 @@
 package com.example.harden_taxes.model
 
-data class Apartment ()
+data class Apartment (
+    val firstName:String?,
+    val lastName:String?,
+    val email:String?,
+    val telefone:Int?,
+    val floor:Int
+        )
